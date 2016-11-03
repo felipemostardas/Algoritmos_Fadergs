fb253e10a632df693440337bdf7e82542377f044
Felipe Santos da Silva <felipesantos@softwarelivre.org>
2016-11-03 19:52:25 -0200

    atualizaçao exercicios

--- FelipeSantos/Exercicios_C++/lista3/lista_3_exercicio_1.cpp
+++ FelipeSantos/Exercicios_C++/lista3/lista_3_exercicio_1.cpp
@@ -11,33 +11,43 @@ Mostrar o resultado da operação realizada. */
 
 main(){
 	setlocale(LC_ALL,"Portuguese");
-	int num1,num2;
-	int calculo;
-	char letra; 
+	int num1=0;
+	int num2=0;
+	int calculo=0;
+	char letra[1]; 
 		printf("digite o primeiro numero:\n ");
 			scanf("%i",&num1);
 		printf("digite o segundo numero:\n ");
 			scanf("%i",&num2);
-		printf ("Digite a opção de calculo ");
-		printf ("A - Adição");
-		printf ("S - Subtração ");
-		printf ("M - Multiplicação ");
-		printf ("D - Divisão ");
-			scanf ("%c",&letra);
-			switch (letra){
+			
+			
+	printf ("\nDigite a opção de calculo ");
+		printf ("\nA - Adição");
+		printf ("\nS - Subtração ");
+		printf ("\nM - Multiplicação ");
+		printf ("\nD - Divisão \n ");
+		printf ("==>");
+//			scanf ("%s",&letra[0]);
+			
+printf("\n%i - %i",num1,num2);			
+/*			switch (letra){
 				
-			case 'A'
+			case 'A' :
 				calculo=num1+num2;
-		printf ("A Adição de %i + %i = %f",num1,num2,calculo);
-			case 'S'
+		printf ("A Adição de %i + %i = %.f",num1,num2,calculo);
+			break;
+			case 'S' :
 				(calculo=num1-num2);
-		printf ("A subtração de %i + %i = %f",num1,num2,calculo);		
-			case 'M'
+		printf ("A subtração de %i + %i = %.f",num1,num2,calculo);		
+			break;
+			case 'M' :
 				calculo=num1*num2;
-		printf ("A Multiplicação de %i + %i = %f",num1,num2,calculo);				
-			case 'D'
+		printf ("A Multiplicação de %i + %i = %.f",num1,num2,calculo);				
+			break;
+			case 'D' :
 				calculo=num1/num2;
-		printf ("A Divisão de %i + %i = %f",num1,num2,calculo);
-}
+		printf ("A Divisão de %i + %i = %.f",num1,num2,calculo);
+			break;
+}*/
 }
 
